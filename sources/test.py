16d021506dcc20ce13a6faae86b88f58fb2863c2
murouc <miurak1978@gmail.com>
2020-05-19 23:16:57 +0900

    submit

---
+++ sources/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print('HTTP/1.0 200 OK')
+print('Content-type: text/html; charset=UTF-8\n\n')
+print('<html>')
+print('<head><title>Test</title></head>')
+print('<body>Test...</body>')
+print('</html>')
