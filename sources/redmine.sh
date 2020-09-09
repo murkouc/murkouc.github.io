842e632a1063288ac2156e6706caa1699f3eb3db
murouc <miurak1978@gmail.com>
2020-09-09 12:41:59 +0900

    submit

--- sources/redmine.sh
+++ sources/redmine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo /home/ubuntu/configuration-yml.sh
 sudo /home/ubuntu/redmine-conf.sh
+sudo service postgresql stop
+sudo service apache2 stop
 sudo service apache2 reload
 sudo service apache2 start
 sudo service postgresql start
